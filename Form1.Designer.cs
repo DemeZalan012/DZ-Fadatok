6adebca3110bf49662ce571f021e6824d675a1ed
Deme Zalán <deme.zalan.janos@diak.szbi-pg.hu>
2025-11-17 08:21:16 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace DZ_Fadatok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.foldrajziadatok = new System.Windows.Forms.Label();
+            this.kilepes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nevlabel = new System.Windows.Forms.Label();
+            this.fovaroslabel = new System.Windows.Forms.Label();
+            this.teruletlabel = new System.Windows.Forms.Label();
+            this.nepesseglabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::DZ_Fadatok.Properties.Resources.AT;
+            this.button1.Location = new System.Drawing.Point(10, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 120);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ausztria";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::DZ_Fadatok.Properties.Resources.FR;
+            this.button2.Location = new System.Drawing.Point(196, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 120);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Franciaország";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = global::DZ_Fadatok.Properties.Resources.GB;
+            this.button3.Location = new System.Drawing.Point(382, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 120);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Nagy-Britannia";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Image = global::DZ_Fadatok.Properties.Resources.HU;
+            this.button4.Location = new System.Drawing.Point(568, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 120);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Magyarország";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Image = global::DZ_Fadatok.Properties.Resources.USA;
+            this.button5.Location = new System.Drawing.Point(754, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 120);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "USA";
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // foldrajziadatok
+            // 
+            this.foldrajziadatok.AutoSize = true;
+            this.foldrajziadatok.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.foldrajziadatok.Location = new System.Drawing.Point(363, 9);
+            this.foldrajziadatok.Name = "foldrajziadatok";
+            this.foldrajziadatok.Size = new System.Drawing.Size(222, 31);
+            this.foldrajziadatok.TabIndex = 5;
+            this.foldrajziadatok.Text = "Földrajzi adatok";
+            // 
+            // kilepes
+            // 
+            this.kilepes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kilepes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.kilepes.Location = new System.Drawing.Point(751, 395);
+            this.kilepes.Name = "kilepes";
+            this.kilepes.Size = new System.Drawing.Size(180, 43);
+            this.kilepes.TabIndex = 6;
+            this.kilepes.Text = "KILÉPÉS";
+            this.kilepes.UseVisualStyleBackColor = true;
+            this.kilepes.Click += new System.EventHandler(this.kilepes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Az ország neve:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fővárosa:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Területe:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Népessége:";
+            // 
+            // nevlabel
+            // 
+            this.nevlabel.AutoSize = true;
+            this.nevlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nevlabel.Location = new System.Drawing.Point(126, 200);
+            this.nevlabel.Name = "nevlabel";
+            this.nevlabel.Size = new System.Drawing.Size(0, 18);
+            this.nevlabel.TabIndex = 11;
+            this.nevlabel.Click += new System.EventHandler(this.nevlabel_Click);
+            // 
+            // fovaroslabel
+            // 
+            this.fovaroslabel.AutoSize = true;
+            this.fovaroslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fovaroslabel.Location = new System.Drawing.Point(126, 250);
+            this.fovaroslabel.Name = "fovaroslabel";
+            this.fovaroslabel.Size = new System.Drawing.Size(0, 18);
+            this.fovaroslabel.TabIndex = 12;
+            this.fovaroslabel.Click += new System.EventHandler(this.fovaroslabel_Click);
+            // 
+            // teruletlabel
+            // 
+            this.teruletlabel.AutoSize = true;
+            this.teruletlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.teruletlabel.Location = new System.Drawing.Point(126, 300);
+            this.teruletlabel.Name = "teruletlabel";
+            this.teruletlabel.Size = new System.Drawing.Size(0, 18);
+            this.teruletlabel.TabIndex = 13;
+            this.teruletlabel.Click += new System.EventHandler(this.teruletlabel_Click);
+            // 
+            // nepesseglabel
+            // 
+            this.nepesseglabel.AutoSize = true;
+            this.nepesseglabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nepesseglabel.Location = new System.Drawing.Point(126, 350);
+            this.nepesseglabel.Name = "nepesseglabel";
+            this.nepesseglabel.Size = new System.Drawing.Size(0, 18);
+            this.nepesseglabel.TabIndex = 14;
+            this.nepesseglabel.Click += new System.EventHandler(this.nepesseglabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 450);
+            this.Controls.Add(this.nepesseglabel);
+            this.Controls.Add(this.teruletlabel);
+            this.Controls.Add(this.fovaroslabel);
+            this.Controls.Add(this.nevlabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kilepes);
+            this.Controls.Add(this.foldrajziadatok);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Országok adatai";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label foldrajziadatok;
+        private System.Windows.Forms.Button kilepes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label nevlabel;
+        private System.Windows.Forms.Label fovaroslabel;
+        private System.Windows.Forms.Label teruletlabel;
+        private System.Windows.Forms.Label nepesseglabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ_Fadatok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Adatok(string orszag, string fovaros, string terulet, string nepesseg)
+        {
+            
+            nevlabel.Text = orszag;
+            fovaroslabel.Text = fovaros;
+            teruletlabel.Text = terulet;
+            nepesseglabel.Text = nepesseg;
+        }
+
+        private void nevlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fovaroslabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teruletlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nepesseglabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Adatok("Ausztria", "Bécs", "83 871 km²", "9,158,750");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Adatok("Franciaország", "Párizs", "632 702 km²", "66 688 524");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Adatok("Nagy-Britannia", "London", "130 279 km²", "58 620 101");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Adatok("Magyarország", "Budapest", "93 026 km²", "9 603 634 ");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            Adatok("USA", "Washington", "9 867 000 km²", "347 963 139");
+        }
+
+        private void kilepes_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+    }
+}
